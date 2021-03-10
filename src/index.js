e148a26d767faf13db2115ee61ec2a732b322b68
Olga <smirnova_olga18@mail.ru>
2021-03-10 18:31:33 +0300

    feat: add function min max avg

--- src/index.js
+++ src/index.js
@@ -1,12 +1,38 @@
 
-exports.min = function min (array) {
-  return 0;
-}
+exports.min = function min(array) {
+  if (array == [] || array == 0 || array == undefined || array == null) {
+    return 0;
+  } else {
+    let minValue;
+    array.sort((a, b) => a - b);
+    minValue = array[0];
+    return minValue;
 
-exports.max = function max (array) {
-  return 0;
-}
+  }
+};
 
-exports.avg = function avg (array) {
-  return 0;
-}
+exports.max = function max(array) {
+  if (array == [] || array == 0 || array == undefined || array == null) {
+    return 0;
+  } else {
+    let maxValue;
+    array.sort((a, b) => a - b);
+    maxValue = array[array.length - 1];
+    return maxValue;
+  }
+};
+
+exports.avg = function avg(array) {
+  if (array == [] || array == 0 || array == undefined || array == null) {
+    return 0;
+  } else {
+    let avgValue = 0;
+    let summ = 0;
+    array.forEach(item => {
+      summ += item;
+    });
+    avgValue = summ / array.length;
+    return avgValue;
+  }
+};
+
